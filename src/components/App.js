37fc918a8fd6102d73c11c597a50a9797e815f5c
Konrad Klimczak <konradklimczak93@gmail.com>
2019-05-02 15:11:04 +0200

    test notification added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React, { Component, Fragment } from "react";
 
 import Navbar from "./Navbar/Navbar";
+import Notification from "./Notification/Notification";
+import { Severity } from "./Severity";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Navbar />
+        <Notification severity={Severity.Info}>
+          Drogi Użytkowniku, Niniejsza strona korzysta z plików cookies dla celów statystycznych i marketingowych.
+          “Dowiedz się więcej” lub kliknij “Akceptuję”, w celu zaakceptowania wszystkich rodzajów cookies.
+        </Notification>
       </Fragment>
     );
   }
