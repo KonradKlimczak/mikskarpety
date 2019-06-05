fd8391fd58dd6ff182e68840e6256e5f84a4e059
Klimczak <e-kdkk@gft.com>
2019-06-05 14:02:38 +0200

    PhotoItem style improvements.

--- src/components/Item/PhotoItem.tsx
+++ src/components/Item/PhotoItem.tsx
@@ -11,7 +11,7 @@ interface IPhotoItemProps {
 
 const PhotoItem: React.SFC<IPhotoItemProps> = (props) => {
   return (
-    <figure className={classnames('photo-item', getSizeClassName(props.size))}>
+    <figure className={classnames('photo-item', getSizeClassName(props.size))} tabIndex={0}>
       <img src={props.src} />
       {props.caption && <figcaption>{props.caption}</figcaption>}
     </figure>
