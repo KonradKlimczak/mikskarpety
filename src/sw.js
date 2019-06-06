41eacc3bda16ae57f8ceb306e95567e181b3952d
Klimczak <e-kdkk@gft.com>
2019-06-06 16:46:28 +0200

    Cache and update

--- src/sw.js
+++ src/sw.js
@@ -1,39 +1,42 @@
-var window = {};
+const CACHE = 'cache-and-update';
 
-self.addEventListener('install', function (event) {
-  // Put `offline.html` page into cache
-  var offlineRequest = new Request('offline.html');
-  event.waitUntil(
-    fetch(offlineRequest).then(function (response) {
-      return caches.open('offline').then(function (cache) {
-        console.log('[oninstall] Cached offline page', response.url);
-        return cache.put(offlineRequest, response);
-      });
-    })
-  );
+const FILES_CACHED = [
+  './index.html',
+  './main.js',
+];
+
+self.addEventListener('install', (event) => {
+  console.log('The service worker is being installed.');
+
+  event.waitUntil(precache());
 });
 
-self.addEventListener('fetch', function (event) {
-  // Only fall back for HTML documents.
-  var request = event.request;
-  // && request.headers.get('accept').includes('text/html')
-  if (request.method === 'GET') {
-    // `fetch()` will use the cache when possible, to this examples
-    // depends on cache-busting URL parameter to avoid the cache.
-    event.respondWith(
-      fetch(request).catch(function (error) {
-        // `fetch()` throws an exception when the server is unreachable but not
-        // for valid HTTP responses, even `4xx` or `5xx` range.
-        console.error(
-          '[onfetch] Failed. Serving cached offline fallback ' +
-          error
-        );
-        return caches.open('offline').then(function (cache) {
-          return cache.match('offline.html');
-        });
-      })
-    );
-  }
-  // Any other handlers come here. Without calls to `event.respondWith()` the
-  // request will be handled without the ServiceWorker.
+
+self.addEventListener('fetch', (event) => {
+  console.log('The service worker is serving the asset.');
+
+  event.respondWith(fromCache(event.request));
+  event.waitUntil(update(event.request));
 });
+
+function precache() {
+  return caches.open(CACHE).then((cache) => {
+    return cache.addAll(FILES_CACHED);
+  });
+}
+
+function fromCache(request) {
+  return caches.open(CACHE).then((cache) => {
+    return cache.match(request).then((matching) => {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function update(request) {
+  return caches.open(CACHE).then((cache) => {
+    return fetch(request).then((response) => {
+      return cache.put(request, response);
+    });
+  });
+}
