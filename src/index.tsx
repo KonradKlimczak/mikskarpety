95a852eb39d218e514f3cf85c1ed35208b42cec5
Klimczak <e-kdkk@gft.com>
2019-06-06 15:30:48 +0200

    Service worker registration.

--- src/index.tsx
+++ src/index.tsx
@@ -19,3 +19,17 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('app'),
 );
+
+// Register a service worker
+
+if ('serviceWorker' in navigator) {
+  (window || {} as any).addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
