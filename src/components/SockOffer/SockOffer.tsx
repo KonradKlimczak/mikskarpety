9bbb2ab7f8d68d3b604d8be60e566d98d209c86c
Klimczak <e-kdkk@gft.com>
2019-06-07 15:20:46 +0200

    Loader for Sock Offer

--- src/components/SockOffer/SockOffer.tsx
+++ src/components/SockOffer/SockOffer.tsx
@@ -1,28 +1,73 @@
 import classnames from 'classnames';
-import * as React from 'react';
+import React, { SFC, Fragment, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { match } from 'react-router';
-
+import m05 from '../../images/pictures/m05.jpg';
 import { ISock } from '../../data/Sock';
 import { IAppState } from '../../reducers';
-import { RemoteData } from '../../utils/RemoteData';
+import { RemoteData, RemoteDataKind } from '../../utils/RemoteData';
+import RemoteLoader from '../../utils/RemoteLoader';
+import PhotoItem from '../Item/PhotoItem';
+import { requestSock, receiveSock } from '../../actions/sock';
+import { Size } from '../../utils/Size';
 
 interface ISockOfferProps {
   socks: RemoteData<ISock[]>;
   match: match<{
     category: string;
   }>;
+  onFetchSocks: () => void;
 }
 
-const SockOffer: React.SFC<ISockOfferProps> = (props) => {
-  return <div className={classnames('sock-offer')}>{props.match.params.category}</div>;
+const SockOffer: SFC<ISockOfferProps> = (props) => {
+  useEffect(() => {
+    if (props.socks.kind === RemoteDataKind.NotAsked) {
+      props.onFetchSocks();
+    }
+  });
+  return (
+    <Fragment>
+      {props.match.params.category}
+      <RemoteLoader remote={props.socks}>
+        {(data) => (
+          <div className={classnames('sock-offer')}>
+            {data.map((sock) => (
+              <PhotoItem size={Size.Big} src={sock.src} caption={sock.name} />
+            ))}
+          </div>
+        )}
+      </RemoteLoader>
+    </Fragment>
+  );
 };
 
 export default connect(
   (state: IAppState) => {
     return {
-      sock: state.sock.list,
+      socks: state.sock.list,
     };
   },
-  () => ({}),
+  (dispatch) => ({
+    onFetchSocks: () => {
+      dispatch(requestSock());
+      new Promise((resolve) => {
+        resolve();
+      }).then(() => {
+        dispatch(
+          receiveSock([
+            {
+              id: '',
+              colors: [],
+              description: '',
+              name: 'Skarpety garniturowe',
+              sizes: [],
+              tags: [],
+              packing: 10,
+              src: m05,
+            },
+          ]),
+        );
+      });
+    },
+  }),
 )(SockOffer);
